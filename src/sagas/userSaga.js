647124d3153f7fedefc8ddaed4fa2cb0a80cf9d1
duongvancuong <vancuong279dn@gmail.com>
2018-03-26 20:58:54 +0700

    make saga for user

---
+++ src/sagas/userSaga.js
@@ -0,0 +1,14 @@
+import { put, call } from 'redux-saga/effects';
+import { login } from '../services/user';
+import * as types from '../constants/actionTypes';
+
+export function* loginUser(data) {
+  try {
+    const auth_token = yield call(login, data);
+    yield [
+      put({type: types.AUTHENTICATED, auth_token}),
+    ];
+  } catch (error) {
+    yield put({type: types.AUTHENTICATE_ERROR, error});
+  }
+}
