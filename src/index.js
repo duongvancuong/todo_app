da1dd57fd88047c40950dbf62054483f881594c3
duongvancuong <vancuong@279dn@gmail.com>
2018-05-18 13:52:14 +0700

    Implement react-router-redux

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux'
 import { render } from 'react-dom';
-import { Router } from 'react-router-dom';
+import createHashHistory from 'history/createHashHistory';
+import { ConnectedRouter } from 'react-router-redux';
+
 import routes from './routes';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/configureStore';
-import createHashHistory from 'history/createHashHistory';
 
 const history = createHashHistory();
 const store = configureStore();
 
 render(
   <Provider store={store}>
-    <Router history={history} children={routes} />
+    <ConnectedRouter history={history} children={routes} />
   </Provider>,
   document.getElementById('root')
 );
