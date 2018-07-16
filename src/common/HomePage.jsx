898d25dc333ccde75360c1d27513a7d0f02c9243
duongvancuong <vancuong@279dn@gmail.com>
2018-07-16 17:16:25 +0700

    Tabs Component

--- src/common/HomePage.jsx
+++ src/common/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Tabs from '../components/Tabs';
 
 const divStyle = {
   color: 'red',
@@ -8,6 +9,17 @@ const divStyle = {
 const HomePage = () => (
   <div className="jumbotron center">
     <h1 style={divStyle} className="lead"> Welcome! </h1>
+    <Tabs>
+        <div label="すべて">
+          See ya later, <em>Alligator</em>!
+        </div>
+        <div label="法人">
+          After &apos;while, <em>Crocodile</em>!
+        </div>
+        <div label="個人">
+          Nothing to see here, this tab is <em>extinct</em>!
+        </div>
+      </Tabs>
   </div>
 );
 
